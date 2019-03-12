e859b5fd36723172e2a0c93d94f97b4f91068ed0
plapicola <p.lapicola@gmail.com>
2019-03-12 14:15:29 -0600

    Adds merchant invoice relationship and adds usage of serializer to merchant controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/merchants/find', to: 'merchants/search#show', as: :find_merchant
-      get '/merchants/find_all', to: 'merchants/search#index', as: :find_all_merchant
+      namespace :merchants do
+        get '/find', to: 'search#show', as: :find
+        get '/find_all', to: 'search#index', as: :find_all
+      end
       resources :merchants, only: [:index, :show]
     end
   end
