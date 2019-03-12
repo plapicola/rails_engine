fba71af20b1fe0aae6ca95d88d9421475f371559
plapicola <p.lapicola@gmail.com>
2019-03-12 13:34:49 -0600

    Adds find_all functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#show', as: :find_merchant
+      get '/merchants/find_all', to: 'merchants/search#index', as: :find_all_merchant
       resources :merchants, only: [:index, :show]
     end
   end
