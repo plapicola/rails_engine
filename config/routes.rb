dc9c70c6796d1a2adaac4865a81fe307f251db0f
plapicola <p.lapicola@gmail.com>
2019-03-12 11:40:07 -0600

    Adds endpoint for merchant finder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants/search#show', as: :find_merchant
       resources :merchants, only: [:index, :show]
     end
   end
