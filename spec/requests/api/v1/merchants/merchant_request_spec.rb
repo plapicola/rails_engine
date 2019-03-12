895d7925ff74bb7b84d3e8836d4e9fcca679c24f
plapicola <p.lapicola@gmail.com>
2019-03-12 11:25:56 -0600

    Adds tests for merchant finder function

--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -20,4 +20,52 @@ describe 'Merchants API' do
     expect(response).to be_successful
     expect(merchant["id"]).to eq(id)
   end
+
+  it 'can find a merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/find?id=#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it 'can find a merchant by its name' do
+    name = create(:merchant).name
+
+    get "/api/v1/merchants/find?name=#{name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["name"]).to eq(name)
+  end
+
+  it 'can find a merchant by its created_at time' do
+    time_parameter = "2012-03-27T14:54:05.000Z"
+    created_at = "2012-03-27 14:54:05 UTC"
+    create(:merchant, created_at: created_at)
+
+    get "/api/v1/merchants/find?created_at=#{created_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["created_at"]).to eq(time_parameter)
+  end
+
+  it 'can find a merchant by its updated_at time' do
+    time_parameter = "2012-03-27T14:54:05.000Z"
+    updated_at = "2012-03-27 14:54:05 UTC"
+    create(:merchant, updated_at: updated_at)
+
+    get "/api/v1/merchants/find?updated_at=#{updated_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["updated_at"]).to eq(time_parameter)
+  end
 end
