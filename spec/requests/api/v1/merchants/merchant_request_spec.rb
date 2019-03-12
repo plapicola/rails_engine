50d12b791d8d464758ee6fed6cfa606629df5680
plapicola <p.lapicola@gmail.com>
2019-03-11 19:02:15 -0600

    Adds tests for basic merchant api endpoints

---
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends a list of all merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants/'
+
+    expect(response).to be_successful
+    expect(JSON.parse(response.body).count).to eq(3)
+  end
+
+  it 'can get a merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
+end
