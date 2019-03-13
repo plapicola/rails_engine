768c4fc65f59b01766dcde11509b8189dae2eb27
plapicola <p.lapicola@gmail.com>
2019-03-12 18:26:23 -0600

    Adds routing and controller action for top merchant by items endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show', as: :find
         get '/find_all', to: 'search#index', as: :find_all
         get '/most_revenue', to: 'most_revenue#index', as: :most_revenue
+        get '/most_items', to: 'most_items#index', as: :most_items
       end
       resources :merchants, only: [:index, :show] do
         get '/items', to: 'merchants/items#index', as: :items
