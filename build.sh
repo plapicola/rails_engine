83635ce2bbe1bf2a1274749e538d65215473f03d
plapicola <p.lapicola@gmail.com>
2019-04-25 13:58:16 -0600

    Updates build script

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ docker rm rails_engine_app
 # Create containers
 
 docker create --hostname db --name rails_engine_db --net rails_engine_1 --net-alias db db
-docker create --hostname app --name rails_engine_app --net rails_engine_1 --net-alias app app
+docker create --hostname app --name rails_engine_app --net rails_engine_1 --net-alias app -p 80:3000 app
 
 # Start containers
 
@@ -34,6 +34,8 @@ docker start rails_engine_db rails_engine_app
 docker exec rails_engine_app rails db:create
 docker exec rails_engine_app rails db:migrate
 docker exec rails_engine_app rails db:seed
+echo "Populating sample database... (This may take a while)"
+docker exec rails_engine_app rake import
 
 # Notify build completed
 
